31caf118f2102cf7b1a4574b5518917c65307385
Rafał <rafal.wrzesniak94@gmail.com>
2021-12-20 15:57:18 +0100

    Moves to ResponseEntity usage

--- src/main/java/sample/coding/task/myapp/controller/CaseResource.java
+++ src/main/java/sample/coding/task/myapp/controller/CaseResource.java
@@ -2,6 +2,7 @@ package sample.coding.task.myapp.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sample.coding.task.myapp.model.rmdb.Note;
 import sample.coding.task.myapp.model.rmdb.Report;
@@ -9,6 +10,7 @@ import sample.coding.task.myapp.service.CaseService;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.OK;
 import static sample.coding.task.myapp.model.rmdb.Report.Status;
 
 @Slf4j
@@ -19,38 +21,38 @@ public class CaseResource {
     private final CaseService caseService;
 
     @GetMapping("/reports")
-    List<Report> getAllCases() {
-        return caseService.getAllCases();
+    ResponseEntity<List<Report>> getAllCases() {
+        return new ResponseEntity<>(caseService.getAllCases(), OK);
     }
 
     @GetMapping("/reports/status/{status}")
-    List<Report> getCasesWithStatus(@PathVariable Status status) {
-        return caseService.getCasesWithStatus(status);
+    ResponseEntity<List<Report>> getCasesWithStatus(@PathVariable Status status) {
+        return new ResponseEntity<>(caseService.getCasesWithStatus(status), OK);
     }
 
     @GetMapping("/reports/user/{userId}")
-    List<Report> getOpenCases(@PathVariable Integer userId) {
-        return caseService.getOpenCases(userId);
+    ResponseEntity<List<Report>> getOpenCases(@PathVariable Integer userId) {
+        return new ResponseEntity<>(caseService.getOpenCases(userId), OK);
     }
 
     @GetMapping("/reports/user/{userId}/status/{status}")
-    List<Report> getOpenCases(@PathVariable Integer userId, @PathVariable Status status) {
-        return caseService.getOpenCases(userId, status);
+    ResponseEntity<List<Report>> getOpenCases(@PathVariable Integer userId, @PathVariable Status status) {
+        return new ResponseEntity<>(caseService.getOpenCases(userId, status), OK);
     }
 
     @GetMapping("/report/{reportId}")
-    Report getCase(@PathVariable Integer reportId) {
-        return caseService.getCase(reportId);
+    ResponseEntity<Report> getCase(@PathVariable Integer reportId) {
+        return new ResponseEntity<>(caseService.getCase(reportId), OK);
     }
 
     @PostMapping(value = "/report/create", consumes = "application/json")
-    Report createCase(@RequestBody Report toCreate) {
-        return caseService.createCase(toCreate);
+    ResponseEntity<Report> createCase(@RequestBody Report toCreate) {
+        return new ResponseEntity<>(caseService.createCase(toCreate), OK);
     }
 
     @PostMapping(value = "/report/{reportId}/addNote", consumes = "text/plain")
-    Note addNote(@PathVariable Integer reportId, @RequestBody String detail) {
-        return caseService.addNoteToCase(reportId, detail);
+    ResponseEntity<Note> addNote(@PathVariable Integer reportId, @RequestBody String detail) {
+        return new ResponseEntity<>(caseService.addNoteToCase(reportId, detail), OK);
     }
 
 
